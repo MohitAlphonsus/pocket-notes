416b65934a5a114d16650b0c3528940e2c553b86
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-20 19:44:37 +0530

    main app component where all children files are imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { Sidebar, Content } from './components';
+
 export default function App() {
-	return <main className="app"></main>;
+	return (
+		<main className="app">
+			<Sidebar />
+			<Content />
+		</main>
+	);
 }
