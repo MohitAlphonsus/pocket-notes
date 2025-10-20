1d205b844bc757aba926f9f4f9108a8e55679270
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-20 22:12:38 +0530

    modal form opens on button click, which is in sidebar section

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,22 @@
+import { useState } from 'react';
+import { GroupLIst, Modal, FormAddGroup } from '../components';
 import styles from './Sidebar.module.css';
 
-const arr = [
-	{ id: 1, name: 'Html Group' },
-	{ id: 2, name: 'Html Group' },
-	{ id: 3, name: 'Html Group' },
-	{ id: 4, name: 'Html Group' },
-	{ id: 5, name: 'Html Group' },
-	{ id: 6, name: 'Html Group' },
-	{ id: 7, name: 'Html Group' },
-	{ id: 8, name: 'Html Group' },
-	{ id: 9, name: 'Html Group' },
-	{ id: 10, name: 'Html Group' },
-];
 export default function Sidebar() {
+	const [showModal, setShowModal] = useState(false);
+
 	return (
 		<aside className={styles.sidebar}>
 			<header className={styles.header}>
-				<h1>Pocket Notes</h1>
+				<h2>Pocket Notes</h2>
 			</header>
-			<ul className={styles.list}>
-				{arr.map(note => (
-					<li key={note.id}>
-						<span>
-							{note.name.split(' ')[0][0] + note.name.split(' ')[1][0]}
-						</span>
-						<span>{note.name}</span>
-					</li>
-				))}
-			</ul>
-			<button className={styles.btnAdd}>+</button>
+			<GroupLIst />
+			<button className={styles.btnAdd} onClick={() => setShowModal(true)}>
+				+
+			</button>
+			<Modal showModal={showModal} onClose={() => setShowModal(false)}>
+				<FormAddGroup />
+			</Modal>
 		</aside>
 	);
 }
