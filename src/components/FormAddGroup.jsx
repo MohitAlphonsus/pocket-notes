eac04475a8e982d6b4157ec6a1366a48993cce89
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-20 23:36:57 +0530

    modal form color selection fuctionality added, UI updated

--- src/components/FormAddGroup.jsx
+++ src/components/FormAddGroup.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './FormAddGroup.module.css';
 
 const colors = [
@@ -8,9 +9,29 @@ const colors = [
 	'#0047FF',
 	'#6691FF',
 ];
-export default function FormAddGroup() {
+export default function FormAddGroup({ onClose }) {
+	const [addNoteGroupData, setAddNoteGroupData] = useState({
+		groupName: '',
+		color: '',
+	});
+
 	function handleSubmitGroupCreation(e) {
 		e.preventDefault();
+		if (
+			addNoteGroupData.groupName.trim() === '' ||
+			addNoteGroupData.color === ''
+		)
+			return;
+
+		onClose();
+	}
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setAddNoteGroupData({
+			...addNoteGroupData,
+			[name]: value,
+		});
 	}
 
 	return (
@@ -19,19 +40,40 @@ export default function FormAddGroup() {
 			<form className={styles.form} onSubmit={handleSubmitGroupCreation}>
 				<div className={styles.formGroup}>
 					<label htmlFor="groupName">Group Name</label>
-					<input type="text" id="groupName" placeholder="Enter group name" />
+					<input
+						type="text"
+						id="groupName"
+						placeholder="Enter group name"
+						onChange={handleChange}
+						name="groupName"
+						value={addNoteGroupData.groupName}
+					/>
 				</div>
 				<div className={styles.formGroup}>
 					<span>Choose Color</span>
 					<div className={styles.colors}>
 						{colors.map(color => (
-							<div
+							<label
 								key={color}
-								style={{ backgroundColor: `${color}` }}
+								htmlFor={color}
 								className={styles.color}
+								style={{
+									backgroundColor: color,
+									outline: `2px solid ${color}`,
+								}}
 							>
-								&nbsp;
-							</div>
+								<input
+									id={color}
+									type="radio"
+									style={{
+										opacity: 0,
+									}}
+									name="color"
+									checked={addNoteGroupData.color === color}
+									onChange={handleChange}
+									value={color}
+								/>
+							</label>
 						))}
 					</div>
 				</div>
