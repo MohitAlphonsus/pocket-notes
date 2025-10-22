7d59bbf7ad9280a3c600a7a358f3368fb0083fa1
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-22 09:38:39 +0530

    context for notes state is created

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -1,17 +1,30 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from 'react';
 
 const NotesContext = createContext();
 
 function NotesProvider({ children }) {
-  return <NotesContext.Provider value={}>{children}</NotesContext.Provider>;
+	const [notes, setNotes] = useState([]);
+
+	function handleAddTitleToNotes(title, color) {
+		setNotes(prevNotes => [
+			{ id: Date.now(), title, color, notes: [] },
+			...prevNotes,
+		]);
+	}
+
+	return (
+		<NotesContext.Provider value={{ notes, onAddTitle: handleAddTitleToNotes }}>
+			{children}
+		</NotesContext.Provider>
+	);
 }
 
 function useNotes() {
-  const context = useContext(NotesContext);
-  if (context === undefined) {
-    throw new Error("useNotes is used outside NotesProvider");
-  }
-  return context;
+	const context = useContext(NotesContext);
+	if (context === undefined) {
+		throw new Error('useNotes is used outside NotesProvider');
+	}
+	return context;
 }
 
 export { NotesProvider, useNotes };
