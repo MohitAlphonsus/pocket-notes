6baab5f2c4b13a2f58b7c80e7ab3fb4217855b3f
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-22 22:32:11 +0530

    content and hero rendering conditionally

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,26 @@
 import styles from './Content.module.css';
 import heroImg from '../assets/hero.png';
+import { GroupContent } from '../components';
+import { useGroups } from '../context/NotesContext';
 
 export default function Content() {
+	const { currentActiveGroup } = useGroups();
 	return (
 		<section className={styles.content}>
-			<div className={styles.contentBox}>
-				<picture className={styles.heroImg}>
-					<img src={heroImg} alt="illustration art of notes" />
-				</picture>
-				<h1>Pocket Notes</h1>
-				<p>
-					Send and receive messages without keeping your phone online. Use
-					Pocket Notes on up to 4 linked devices and 1 mobile phone
-				</p>
-			</div>
+			{currentActiveGroup ? (
+				<GroupContent />
+			) : (
+				<div className={styles.contentBox}>
+					<picture className={styles.heroImg}>
+						<img src={heroImg} alt="illustration art of notes" />
+					</picture>
+					<h1>Pocket Notes</h1>
+					<p>
+						Send and receive messages without keeping your phone online. Use
+						Pocket Notes on up to 4 linked devices and 1 mobile phone
+					</p>
+				</div>
+			)}
 		</section>
 	);
 }
