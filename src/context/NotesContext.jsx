6b7d5779e689bd0c2481903752315be2157da61d
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-22 23:26:22 +0530

    logic to handle adding notes to group is created

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -9,11 +9,46 @@ function NotesProvider({ children }) {
 	const [activeGroup, setActiveGroup] = useState(null);
 	const currentActiveGroup = groups.find(group => group.id === activeGroup);
 
-	function handleAddTitleToNotes(title, color) {
+	function handleAddTitleToGroups(title, color) {
 		setGroups(prevNotes => [
 			{ id: Date.now(), title, color, notes: [] },
 			...prevNotes,
 		]);
+	}
+
+	function handleAddNotesToGroup(note) {
+		const today = new Date();
+		const formattedDate = today.toLocaleDateString('en-GB', {
+			day: '2-digit',
+			month: 'short',
+			year: 'numeric',
+		});
+
+		const formattedTime = today.toLocaleTimeString('en-US', {
+			hour: '2-digit',
+			minute: '2-digit',
+			hour12: true,
+		});
+
+		setGroups(prevNotes => {
+			return prevNotes.map(group => {
+				if (group.id === activeGroup) {
+					return {
+						...group,
+						notes: [
+							...group.notes,
+							{
+								id: Date.now(),
+								note,
+								datestamp: formattedDate,
+								timestamp: formattedTime,
+							},
+						],
+					};
+				}
+				return group;
+			});
+		});
 	}
 
 	function handleSelectGroup(id) {
@@ -27,14 +62,16 @@ function NotesProvider({ children }) {
 		[groups],
 	);
 
+	console.log(groups);
 	return (
 		<NotesContext.Provider
 			value={{
 				groups,
 				activeGroup,
 				currentActiveGroup,
-				onAddTitle: handleAddTitleToNotes,
+				onAddTitle: handleAddTitleToGroups,
 				onSelectGroup: handleSelectGroup,
+				onAddNotes: handleAddNotesToGroup,
 			}}
 		>
 			{children}
