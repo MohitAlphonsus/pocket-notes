ec5407e6d2b0f15cf5a8aa157103ec5a5e3c4692
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-10-22 22:26:06 +0530

    common imports of components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import GroupLIst from './GroupLIst';
 import Content from './Content';
 import Modal from './Modal';
 import FormAddGroup from './FormAddGroup';
+import GroupContent from './GroupContent';
 
-export { Sidebar, GroupLIst, Content, Modal, FormAddGroup };
+export { Sidebar, GroupLIst, Content, Modal, FormAddGroup, GroupContent };
